35b37cc8638638c1c2ba251939d6aaf09296968e
Syafiq Termizi <73152242+SyafiqTermizi2010@users.noreply.github.com>
2023-02-04 22:35:32 +0800

    Setup repo component

---
+++ src/lib/Repo.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { searchResult } from "../store";
+    import type { Repo } from "../types";
+
+    let repos: Repo[] = [];
+    searchResult.subscribe((sr) => {
+        repos = sr.items;
+    });
+</script>
+
+<div class="row">
+    <div class="col-12">
+        {#each repos as repo}
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">{repo.full_name}</h5>
+                    <p class="card-text">{repo.description}</p>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,15 @@
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
+import type { SearchResult } from "./types";
 
 export const topics = writable<string[]>([]);
+export const searchResult = writable<SearchResult>();
+
+export function search() {
+    const baseURL = "https://api.github.com/search/repositories?q="
+    const searchTopics = get(topics);
+    const queryString = searchTopics.map(topic => `topic:${topic}`).join("+");
+
+    fetch(`${baseURL}${queryString}`)
+        .then(response => response.json())
+        .then(data => searchResult.set(data));
+}
